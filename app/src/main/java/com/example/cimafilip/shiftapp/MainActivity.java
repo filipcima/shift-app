abe711b2e388e028b07519fcd5a48d311de3a4e1
Filip Cima <filip.cima@firma.seznam.cz>
2018-11-26 23:13:32 +0100

    added bottom menu bar

--- app/src/main/java/com/example/cimafilip/shiftapp/MainActivity.java
+++ app/src/main/java/com/example/cimafilip/shiftapp/MainActivity.java
@@ -1,44 +1,87 @@
 package com.example.cimafilip.shiftapp;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DashboardFragment.OnFragmentInteractionListener, ProfileFragment.OnFragmentInteractionListener, ProductionPlanFragment.OnFragmentInteractionListener, MyPlanFragment.OnFragmentInteractionListener, NotificationsFragment.OnFragmentInteractionListener {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        toolbar = getSupportActionBar();
+        toolbar.setTitle("Dashboard");
+        toolbar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#fe485a")));
+//        toolbar.setDisplayShowHomeEnabled(true);
+//        toolbar.setIcon(R.drawable.ic_person_white_24dp);
+
+        loadFragment(new DashboardFragment());
+
+        BottomNavigationView menu = findViewById(R.id.navigation);
+        menu.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_dashboard:
+                    toolbar.setTitle("Dashboard");
+                    fragment = new DashboardFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_production_plan:
+                    toolbar.setTitle("Production plan");
+                    fragment = new ProductionPlanFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_my_plan:
+                    toolbar.setTitle("My plan");
+                    fragment = new MyPlanFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_notifications:
+                    toolbar.setTitle("Notifications");
+                    fragment = new NotificationsFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_profile:
+                    toolbar.setTitle("Profile");
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
+
     }
 
 }
